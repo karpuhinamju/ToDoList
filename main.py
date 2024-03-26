c359248baf546886d09cf4258ad3960c0d73bec6
Мария Карпухина <m.karpukhina11@gmail.com>
2024-03-26 21:29:16 +0300

    added class Task

--- main.py
+++ main.py
@@ -1,19 +1,32 @@
 import datetime
 class Task:
-    id = 0
-    name = ""
-    description = ""
-    deadline = datetime.datetime
-
-
+    counter = 0
+    def __init__(self, name, description, deadline):
+        self.id = self.counter
+        self.name = name
+        self.description = description
+        self.deadline = deadline
+        self.counter += 1
+    def __str__(self):
+        return f"id={self.id},name={self.name},desc={self.description},dl={self.deadline}"
+def handle_add_task(task_array):
+    name = input("Enter the task's name: ")
+    description = input("Enter the task's description: ")
+    deadline = input("Enter the task's deadline: ")
+    task = Task(name, description, deadline)
+    task_array.append(task)
+tasks = []
 while True:
     command = input("Enter command: ")
     match command:
         case "add_task":
-            print("this is add_task.")
+            handle_add_task(tasks)
         case "help":
             print("this is help")
         case "exit":
             break
+        case "show_tasks":
+            for task in tasks:
+                print(task)
         case _:
             print("unknown command")
