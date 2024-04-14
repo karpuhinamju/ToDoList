267fb28e4e91d0920162aae00aa0f9292a6d08b3
Мария Карпухина <m.karpukhina11@gmail.com>
2024-04-14 10:08:54 +0300

    added commands

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import datetime
+
 class Task:
     counter = 0
     def __init__(self, name, description, deadline):
@@ -6,9 +7,10 @@ class Task:
         self.name = name
         self.description = description
         self.deadline = deadline
+        self.is_done = False
         Task.counter += 1
     def __str__(self):
-        return f"id={self.id},name={self.name},desc={self.description},dl={self.deadline}"
+        return f"id={self.id},name={self.name},desc={self.description},dl={self.deadline},done={self.is_done}"
 def handle_add_task(task_array):
     name = input("Enter the task's name: ")
     description = input("Enter the task's description: ")
@@ -16,20 +18,57 @@ def handle_add_task(task_array):
     task = Task(name, description, deadline)
     task_array.append(task)
 tasks = []
+tasks.append(Task("n1", "d1", "dl1"))
+tasks.append(Task("n2", "d2", "dl2"))
+tasks.append(Task("n3", "d3", "dl3"))
 while True:
-    tasks.append(Task("n1", "d1", "dl1"))
-    tasks.append(Task("n2", "d2", "dl2"))
-    tasks.append(Task("n3", "d3", "dl3"))
     command = input("Enter command: ")
     match command:
-        case "add_task":
+        case "add_task"|"a":
             handle_add_task(tasks)
-        case "help":
+        case "help"|"h":
             print("this is help")
-        case "exit":
+        case "exit"|"q":
             break
-        case "show_tasks":
+        case "show_tasks"|"s":
             for task in tasks:
+                print(task)
+        case "delete_by_id"|"d":
+            i = 0
+            iddel = int(input("Введите id задачи: "))
+            while i < len(tasks):
+                if tasks[i].id == iddel:
+                    del tasks[i]
+                    break
+                i += 1
+        case "resolve_task"|"rt":
+            i = 0
+            id = int(input("Введите id задачи"))
+            while i < len(tasks):
+                if tasks[i].id == id:
+                    tasks[i].is_done = True
+                    break
+                i += 1
+        case "delete_resolved"|"dr":
+            i = 0
+            isd_tasks = []
+            while i < len(tasks):
+                if tasks[i].is_done:
+                    isd_tasks.append(tasks[i])
+                    del tasks[i]
+                    i -= 1
+                i += 1
+                for task in isd_tasks:
+                     print("the task with id",task.id, "has been deleted")
+            print(len(isd_tasks), "task(s) have been deleted")
+        case "show_unresolved"|"su":
+            unr_tasks = []
+            i = 0
+            while i < len(tasks):
+                if tasks[i].is_done == False:
+                    unr_tasks.append(tasks[i])
+                i += 1
+            for task in unr_tasks:
                 print(task)
         case _:
             print("unknown command")
